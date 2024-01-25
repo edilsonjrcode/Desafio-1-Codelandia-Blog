d25b09010501998885c8d1a91ecc32be233a6e40
edilsonjrcode <2022111510116@iesp.edu.br>
2024-01-24 22:34:52 -0300

    filtro e like

--- src/js/scripts/script.js
+++ src/js/scripts/script.js
@@ -1,12 +1,43 @@
+//Like
 {
-    let icon_heart_active = "src/assets/icon-heart-active.svg"
-    let icon_heart = "src/assets/icon-heart.svg"
+  let icon_heart_active = "src/assets/icon-heart-active.svg";
+  let icon_heart = "src/assets/icon-heart.svg";
+  const hearts = document.querySelectorAll(".icon-heart");
 
-    function changeIcon(){
+  hearts.forEach((item) => {
+    item.addEventListener("click", () => {
+      if (item.getAttribute("src") == "src/assets/icon-heart.svg") {
+        item.setAttribute("src", icon_heart_active);
+      } else {
+        item.setAttribute("src", icon_heart);
+      }
+    });
+  });
+}
 
-      document.getElementById("icon-heart").src = icon_heart_active
-      let aux = icon_heart_active;
-      icon_heart_active = icon_heart;
-      icon_heart = aux;
+//Filtro
+{
+  const searchBar = document.querySelector("#search-bar");
+  const cards = document.querySelectorAll(".card-information");
+
+  searchBar.addEventListener("input", search);
+
+  const search = () => {
+    for (let card of cards) {
+      const article = card.querySelector(".informations");
+      console.log(article);
+      const title = article.querySelector("h2").textContent.toLowerCase();
+      if (searchBar.value != "") {
+        if (title.includes(searchBar.value)) {
+          card.style.display = "flex";
+        } else {
+          card.style.display = "none";
+        }
+      } else {
+        for (let card of cards) {
+          card.style.display = "flex";
+        }
+      }
     }
+  };
 }
